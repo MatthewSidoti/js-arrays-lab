f047aaade91c4bfe0d9f32bb7631ba81f220db79
MatthewSidoti <coding.sidoti@gmail.com>
2024-06-04 22:29:17 -0400

    exersice 8

--- script.js
+++ script.js
@@ -21,21 +21,116 @@ Complete Exercise 2 in the space below:
 */
 
 
-const foods = ['pizza', 'cheeseburger'];  
+const foodsTwo = [];
+foodsTwo.push('pizza', 'cheeseburger')  
 
-console.log(foods);
+console.log(foodsTwo);
+
+
+// Exercise 3: Insert at the beginning
+
+// 1) Insert the string 'taco' at the beginning of the `foods` array.
+
+// Complete Exercise 3 in the space below:
+
+const foodsThree = ['pizza', 'cheeseburger'];  
+foodsThree.unshift('taco')
+console.log(foodsThree);
+
+
+
+// console.log('Exercise 3 result:', foods);
+
+
+// Exercise 4: Access an array element
+
+// 1) Retrieve the 'pizza' string from the array based on its position (index) in
+//    the array.  
+
+// 2) Assign it to a variable called `favFood`.
+
+// Complete Exercise 4 in the space below:
+// */
+const foodsFour = ['taco', 'pizza', 'cheeseburger'];
+console.log(foodsFour[1])
+
+
+// console.log('Exercise 4 result:', favFood);
+
 
 /*
-Exercise 3: Insert at the beginning
+Exercise 5: Insert an element between two others
 
-1) Insert the string 'taco' at the beginning of the `foods` array.
+1) Insert the string 'tofu' between 'pizza' and 'cheeseburger' in the array.
 
-Complete Exercise 3 in the space below:
+Complete Exercise 5 in the space below:
 */
-const foods = ['pizza', 'cheeseburger'];  
-
-console.log(foods.shiftgit('taco'));
-
+const foodsFive = ['taco', 'pizza', 'cheeseburger'];  
+foodsFive.splice(1,0,'tofu' )
 
 
-console.log('Exercise 3 result:', foods);
+console.log(foodsFive);
+
+
+/*
+Exercise 6: Replace elements
+
+1) Replace 'pizza' in the `foods` array with 'sushi' and 'cupcake'.
+
+Complete Exercise 6 in the space below:
+*/
+const foodsSix =[ 'taco', 'tofu', 'pizza', 'cheeseburger' ]
+foodsSix.splice(2, 1, 'sushi', 'cupcake')
+
+console.log(foodsSix);
+
+
+// console.log('Exercise 6 result:', foods);
+
+
+/*
+Exercise 7: Using the `slice()` method
+
+1) Use the `slice()` method to create a new array that contains 'sushi' and 
+   'cupcake'.
+
+2) Assign it to a variable named `yummy`.
+
+Complete Exercise 7 in the space below:
+*/
+const foodsSeven = [ 'taco', 'tofu', 'sushi', 'cupcake', 'cheeseburger' ]
+console.log(foodsSeven.slice(2,4));
+
+// console.log('Exercise 7 result:', yummy);
+
+/*
+Exercise 8: Finding an index
+
+1) Using the `indexOf()` method, find the index of the string 'tofu' in the 
+   `foods` array. 
+
+2) Assign it to a variable named `soyIdx`.
+
+Complete Exercise 8 in the space below:
+*/
+
+const foodsEight = [ 'taco', 'tofu', 'sushi', 'cupcake', 'cheeseburger' ]
+console.log(foodsEight.indexOf('tofu'));
+
+/*
+Exercise 9: Joining elements
+
+1) Use the `join()` method to concatenate the strings in the `foods` array, 
+   separated by ' -> '. 
+
+2) Assign the result to a variable called `allFoods`. 
+
+Note: The final result should log as:
+'taco -> sushi -> cupcake -> tofu -> cheeseburger'
+
+Complete Exercise 9 in the space below:
+*/
+
+
+
+console.log('Exercise 9 result:', allFoods);
